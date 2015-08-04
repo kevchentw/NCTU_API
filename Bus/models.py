66085ba9b795581ce830be1f52a0c38f2aece9d9
Kevin Chen <me@kevchentw.com>
2015-08-04 20:06:12 +0800

    update bus

--- Bus/models.py
+++ Bus/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 
 class BusStop(models.Model):
-    name = models.CharField(max_length=50)
+    name_ZH = models.CharField(max_length=50)
+    name_EN = models.CharField(max_length=50)
+    description_ZH = models.TextField()
+    description_EN = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class BusRoute(models.Model):
-    name = models.CharField(max_length=50)
+    name_ZH = models.CharField(max_length=50)
+    name_EN = models.CharField(max_length=50)
+    departure = models.ForeignKey('BusStop',related_name='person_book')
+    destination = models.ForeignKey('BusStop')
+    provider = models.CharField(max_length=50)
+    description_ZH = models.TextField()
+    description_EN = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,5 +26,7 @@ class BusSchedule(models.Model):
     time = models.TimeField()
     route = models.ForeignKey('BusRoute')
     stop = models.ForeignKey('BusStop')
+    description_ZH = models.TextField()
+    description_EN = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
